04444770ffcde14b7636f6a2aab42cf023601ae6
Abhishek kadian <ak5374@nyu.edu>
2016-04-12 13:44:56 -0400

    Updated to get headlines for Feburary

--- get_headlines.py
+++ get_headlines.py
@@ -1,42 +1,49 @@
-# Script name    : get_headlines.py
-# Author         : Abhishek kadian
-# Created        : April 8, 2016
-# Last Modified  : 
-# Version        : 1.0
+'''Script name : get_headlines.py
+Author         : Abhishek kadian
+Created        : April 8, 2016
+Last Modified  : April 12,2016
+Version        : 1.0'''
 
 
-# Description: download news headlines from yahoo finance! The headlines|links are used to scrape news articles
+'''Description: download news headlines from yahoo finance!
+The headlines|links are used to scrape news articles'''
 
 import requests
-import re
 import io
+import time
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
-companyname = raw_input("enter the company name : ")
+companyName = raw_input("enter the Stock name : ")
 
-searchurl = 'http://finance.yahoo.com/q?s='+companyname
+def gethealine(companyName, day):
+    "get the headlines"
+    date = '2016-02-'+str(day)
+    searchUrl = 'http://finance.yahoo.com/q/h?s='+companyName+'&t='+date
+    #use fake useragent
+    ua = UserAgent();
+    head = ua.firefox.encode('ascii', 'ignore')
+    headers = {'useragent':head}
+    response = requests.get(searchUrl, headers=headers)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    links = soup.select('div.yfi_quote_headline ul > li > a')
 
-#use fake useragent
+    '''write the search results in file, a new file for each day'''
+    
+    filename = 'links'+str(day)+'.txt'
+    with io.open(filename, encoding='utf-8', mode='w+') as ns:
+        for link in links:
+	        ns.write(link.get('href')+'\n')
+        ns.close()
 
-ua = UserAgent();
 
-head = ua.firefox.encode('ascii','ignore')
-headers = {'useragent':head}
+#run the loop for 29 days: Month of Feburary
+day = 29
+while day > 0:
+    gethealine(companyName, day)
+    time.sleep(5)
+    day = day-1
 
-response = requests.get(searchurl, headers = headers)
-
-content  = response.content
-
-#parse
-soup = BeautifulSoup(content,'html.parser')
-links = soup.select('div.yfi_quote_headline ul > li > a')
-
-print "search successful"
-print "printing the search results in file"
-
-with io.open('links.txt',encoding='utf-8', mode='w+') as ns:
-	for link in links:
-		ns.write(link.get('href')+'\n')
-
+print "Successful. All headlines have been saved to disk"
